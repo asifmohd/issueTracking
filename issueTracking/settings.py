cbe74fdff7aacf99d10455cd5920fd413f0f7ef3
Mohammad Asif <arshadasif0312@gmail.com>
2021-12-23 15:10:13 +0530

    Resolve MIDDLEWARE_CLASSES deprecation in 1.10

--- issueTracking/settings.py
+++ issueTracking/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = (
     'jquery',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
