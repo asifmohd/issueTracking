ad2d4f6d6a6572f269e5839076191e1345edbb82
Mohammad Asif <arshadasif0312@gmail.com>
2021-12-23 14:31:02 +0530

    Enable middleware config required in Django 1.10.0

--- issueTracking/settings.py
+++ issueTracking/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'issueTracking.urls'
